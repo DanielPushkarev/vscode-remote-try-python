945587b0d4803bae823e0b9651376a7fb9ae23d0
DanielPushkarev <128053891+DanielPushkarev@users.noreply.github.com>
2023-08-25 20:32:52 +0700

    Add files via upload

---
+++ big_project/big_project/big_project1/news/migrations/0002_rename_posts_category.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='post',
+            old_name='posts',
+            new_name='post_category',
+        ),
+    ]
